59e1a34a71eeb4c51c62f88417b3205b667621f6
niikkiita <86122796+niikkiita@users.noreply.github.com>
2021-06-19 10:59:36 +0530

    Update config.php
    updated

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-	$servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "bank";
+	$servername = "ec2-3-212-75-25.compute-1.amazonaws.com";
+        $username = "svgoqrbumwusoc";
+        $password = "d2e316a5a8665a0af2acc7bb5f0395d00472cbe10fb913c6c58228d203352c37";
+        $database = "de169a6mifg8nd";
 
 // Create connection
         $conn = mysqli_connect($servername, $username, $password, $database);
